dfed7f420c16b01d4d77f2773883a6abfac2ce98
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-06-14 08:42:11 +0900

    [add] bestTime, stopped status

--- popup.js
+++ popup.js
@@ -1,25 +1,34 @@
 let lastStartTime = new Date();
 let lastEndTime = new Date();
 let lastLoadTime = new Date();
-var autoReloadButton = document.getElementById('enable-loop');
+let bestTime;
+const autoReloadButton = document.getElementById('enable-loop');
+const stopRecordButton = document.getElementById('stop-record');
 
 const formatDate = (date) => {
     // return ''
     //  + ('' + date.getUTCDate()) not working
-    return ('00' + (date.getUTCDate() - 1)) + ":"
-    + ('00' + date.getUTCHours()).slice(-2) + ":"
-    + ('00' + date.getUTCMinutes()).slice(-2) + ":"
-    + ('00' + date.getUTCSeconds()).slice(-2) + ":"
-    + ('00' + date.getUTCMilliseconds()).slice(-3);
+    return ((date.getUTCDate() - 1) > 0 ? ('00' + (date.getUTCDate() - 1)).slice(-2) + ":" : '')
+        + ('00' + date.getUTCHours()).slice(-2) + ":"
+        + ('00' + date.getMinutes()).slice(-2) + ":"
+        + ('00' + date.getSeconds()).slice(-2) + ":"
+        + ('00' + date.getMilliseconds()).slice(-3);
 }
 
 const updateClock = async () => {
     const strg = chrome.storage.local;
     const clock = document.getElementById('realtime-clock');
+    const bestTimeEle = document.getElementById('best-time');
+
     lastStartTime = await strg.get(['lastStartTime']).then((result) => new Date(Number(result.lastStartTime)));
     lastEndTime = await strg.get(['lastEndTime']).then((result) => new Date(Number(result.lastEndTime)));
     lastLoadTime = await strg.get(['lastLoadTime']).then((result) => new Date(Number(result.lastLoadTime)));
+    bestTime = await strg.get(['bestTime']).then((result) => new Date(Number(result.bestTime)));
+
     let status = await strg.get(['status']).then((result) => result.status);
+
+    console.debug('bestTime: ', bestTime);
+    bestTimeEle.innerText = formatDate(bestTime);
 
     if (status === 'loading') {
         let nowLoadTime = new Date() - lastStartTime;
@@ -32,6 +41,15 @@ const updateClock = async () => {
         clock.innerText = formatDate(lastLoadTime);
         return;
     }
+
+    if (status === 'stopped') {
+        clock.innerText = "記録を中止しました。";
+        return;
+    }
+
+    clock.innerText = "クリックして記録を開始します。"
+    
+
     
     // console.debug('lastStartTime: ', lastStartTime);
 }
@@ -58,7 +76,17 @@ const reloadAutoReload = async () => {
     reloadAutoReload();
 }
 
+const stopRecording = async () => {
+    const strg = chrome.storage.local;
+    strg.get(['status']).then((result) => {
+        if (result.status === 'loading') {
+            strg.set({'status': 'stopped'});
+        }
+    });
+}
+
 setInterval(updateClock, 47);
 reloadAutoReload();
 
 autoReloadButton.addEventListener('click', toggleAutoReload);
+stopRecordButton.addEventListener('click', stopRecording);
